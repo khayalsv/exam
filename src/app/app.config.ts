4599e6745d6edb426eb3c601eeaeae3f80b71512
ApertechProjects <selimovxeyal@gmail.com>
2025-07-03 16:46:31 +0400

    structure package

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,30 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community';
+import { MyPreset } from './mypreset';
+ModuleRegistry.registerModules([AllCommunityModule]);
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: MyPreset,
+        options: {
+          darkModeSelector: '.dark',
+          cssLayer: {
+            name: 'primeng',
+            order: 'tailwind-base, primeng, tailwind-utilities'
+          }
+        }
+      }
+    })
+
   ]
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,80 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { AgGridAngular } from 'ag-grid-angular';
+import { ColDef, themeQuartz } from 'ag-grid-community';
+import { DividerModule } from 'primeng/divider';
+import { ToggleButtonModule } from 'primeng/togglebutton';
 import { RouterOutlet } from '@angular/router';
+import { CardModule } from 'primeng/card';
+import { CommonModule } from '@angular/common';
+import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MenubarModule } from 'primeng/menubar';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  imports: [ButtonModule, AgGridAngular,
+    RouterOutlet, ToggleButtonModule, DividerModule, ButtonModule,
+    CardModule, CommonModule, InputTextModule, ReactiveFormsModule, FormsModule, MenubarModule
+  ],
+  templateUrl: 'app.html',
 })
 export class App {
   protected title = 'app';
+
+  theme = themeQuartz;
+
+  rowData = [
+    { make: "Tesla", model: "Model Y", price: 64950, electric: true },
+    { make: "Ford", model: "F-Series", price: 33850, electric: false },
+    { make: "Toyota", model: "Corolla", price: 29600, electric: false },
+  ];
+
+  colDefs: ColDef[] = [
+    { field: "make" },
+    { field: "model" },
+    { field: "price" },
+    { field: "electric" }
+  ];
+
+  isDarkMode: boolean = false;
+
+  ngOnInit() {
+    const theme = localStorage.getItem('theme');
+    const element = document.querySelector('html');
+    if (element) {
+      if (theme === 'true') {
+        element.classList.add('dark');
+        this.isDarkMode = true;
+      } else {
+        this.isDarkMode = false;
+      }
+    }
+  }
+
+  toggleDarkMode() {
+    const element = document.querySelector('html');
+    if (element) {
+      element.classList.toggle('dark');
+      localStorage.setItem('theme', element.classList.contains('dark') ? 'true' : 'false');
+    }
+  }
+
+  items: MenuItem[];
+
+  constructor() {
+    this.items = [
+      {
+        label: 'About',
+        icon: 'pi pi-fw pi-info-circle',
+        routerLink: ['/']
+      },
+      {
+        label: 'To Do List',
+        icon: 'pi pi-fw pi-envelope',
+        routerLink: ['/to-do-list']
+      }
+    ];
+  }
 }
